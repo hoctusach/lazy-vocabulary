da41d76ef4ad9f81fb7b3ebd48d809294a1154b0
hoctusach <hoctusach@gmail.com>
2025-09-15 15:06:14 +0000

    b

--- docs/sb-drop-custom-words.sql
+++
@@ -1,4 +0,0 @@
-drop table if exists public.custom_words cascade;
-drop function if exists public.upsert_custom_word(text, jsonb) cascade;
-drop function if exists public.delete_custom_word(text, text) cascade;
-notify pgrst, 'reload schema';
